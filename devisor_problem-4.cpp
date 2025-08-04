6c9f999a43e538665039b729879e60109fd85b37
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-08-05 00:04:33 +0600

    Update devisor_problem-4.cpp

--- devisor_problem-4.cpp
+++ devisor_problem-4.cpp
@@ -1,19 +1,24 @@
-        #include<iostream>
-        using namespace std;
-        int main(){
-            int T;
-            cin>>T; //test_case
-    
-        for(int kase=1;kase<=T;kase++){ //outer loop
-            int N;
-            cin>>N; //integer_input
-            cout<<"Case "<<kase<<":"; //for test Cases.
-        for(int i = 1; i <= N; i++){ //inner_loop //calculate the divisor of each N number.
-            if(N%i==0){  
-            cout<<" "<<i;
-         }
-      }
-        cout<<endl;
-     }
-            return 0;
-        }
+         /*iput test case
+               3
+               6
+               15
+               23 */
+                #include<iostream>
+                using namespace std;
+                int main(){
+                    int T;
+                    cin>>T; //test_case
+            
+                for(int kase=1;kase<=T;kase++){ //outer loop
+                    int N;
+                    cin>>N; //integer_input
+                    cout<<"Case "<<kase<<":"; //for test Cases.
+                for(int i = 1; i <= N; i++){ //inner_loop //calculate the divisor of each N number.
+                    if(N%i==0){  
+                    cout<<" "<<i;
+                 }
+              }
+                cout<<endl;
+             }
+                    return 0;
+                }
