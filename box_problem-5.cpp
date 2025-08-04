c5fe4e62e3ff6764e0cad74ae6517f4fb3945c4c
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-08-05 00:03:17 +0600

    Update box_problem-5.cpp

--- box_problem-5.cpp
+++ box_problem-5.cpp
@@ -1,7 +1,13 @@
-        #include<iostream>
-           using namespace std;
-           int main(){
-           int T;
+/*input test case
+     3
+     1
+     3
+     5 */
+
+         #include<iostream>
+         using namespace std;
+         int main(){
+         int T;
            cin>>T;  //test_case
             for(int k=1;k<=T;k++){ 
                int N;
