0d382595114bc13ef3fa1afa921ea43890bfc0c7
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-08-04 23:50:20 +0600

    Update box_problem-5.cpp

--- box_problem-5.cpp
+++ box_problem-5.cpp
@@ -1,19 +1,20 @@
-    #include<iostream>
-       using namespace std;
-       int main(){
-       int T;
-       cin>>T;  //test_case
-        for(int k=1;k<=T;k++){ 
-           int N;
-           cin>>N; //integer input
-   
-       for (int i = 1; i<=N; i++){ //outer_loop //rows
-   
-           for(int j=1;j<=N; j++){ //inner_loop //columns
-             cout<<"*";
-         }
-        cout<<endl;
-        }
-     }
-           return 0;
-       }
+        #include<iostream>
+           using namespace std;
+           int main(){
+           int T;
+           cin>>T;  //test_case
+            for(int k=1;k<=T;k++){ 
+               int N;
+               cin>>N; //integer input
+       
+           for (int i = 1; i<=N; i++){ //outer_loop //rows
+       
+               for(int j=1;j<=N; j++){ //inner_loop //columns
+                 cout<<"*";
+             }
+            cout<<endl;
+          }
+         if (k != T) cout << endl;
+            }
+               return 0;
+           }
